2764b582f85c49559d96b4c51b81a975562dd407
Thomas Coward <thomas01155@gmail.com>
2019-04-02 17:59:06 +0100

    changed spelling mistake

--- src/components/Privacy.js
+++ src/components/Privacy.js
@@ -4,197 +4,152 @@ const Privacy = () => {
   return (
     <div className="jumbotron">
       <div className="alert bg-warning">
-        Please note that your password is stored as plain text, this is due to
-        the way the Hive app requiers a password in plain text. Do not use this
-        service if you disagree with this.
+        Please note that your password is stored as plain text, this is due to the way the Hive API requires a password
+        in plain text. Do not use this service if you disagree with this.
       </div>
       <h1 class="display-4">Privacy Policy</h1>
       <p>
-        This page informs you of our policies regarding the collection, use and
-        disclosure of Personal Information when you use our Service. We will not
-        use or share your information with anyone except as described in this
-        Privacy Policy. We use your Personal Information for providing and
-        improving the Service. By using the Service, you agree to the collection
-        and use of information in accordance with this policy.
+        This page informs you of our policies regarding the collection, use and disclosure of Personal Information when
+        you use our Service. We will not use or share your information with anyone except as described in this Privacy
+        Policy. We use your Personal Information for providing and improving the Service. By using the Service, you
+        agree to the collection and use of information in accordance with this policy.
       </p>
       <p>
-        While using our Service, we may ask you to provide us with certain
-        personally identifiable information that can be used to contact or
-        identify you. Personally identifiable information (“Personal
-        Information”) may include, but is not limited to: email, password,
-        maxTargetTemp and Temperature to set
+        While using our Service, we may ask you to provide us with certain personally identifiable information that can
+        be used to contact or identify you. Personally identifiable information (“Personal Information”) may include,
+        but is not limited to: email, password, maxTargetTemp and Temperature to set
       </p>
       <h3>Log Data</h3>
       <p>
-        We collect information that your browser sends whenever you visit our
-        Service (“Log Data”). This Log Data may include information such as your
-        computer’s Internet Protocol (“IP”) address, browser type, browser
-        version, the pages of our Service that you visit, the time and date of
-        your visit, the time spent on those pages and other statistics.
+        We collect information that your browser sends whenever you visit our Service (“Log Data”). This Log Data may
+        include information such as your computer’s Internet Protocol (“IP”) address, browser type, browser version, the
+        pages of our Service that you visit, the time and date of your visit, the time spent on those pages and other
+        statistics.
       </p>
       <h3>Cookies</h3>
       <p>This site does not set any cookies</p>
       <h3>Compliance With Laws </h3>
-      <p>
-        We will disclose your Personal Information where required to do so by
-        law or subpoena
-      </p>
+      <p>We will disclose your Personal Information where required to do so by law or subpoena</p>
       <h3>Security</h3>
       <p>
-        The security of your Personal Information is important to us, but
-        remember that no method of transmission over the Internet, or method of
-        electronic storage is 100% secure. While we strive to use commercially
-        acceptable means to protect your Personal Information, we cannot
-        guarantee its absolute security. Please remember your password is stored
-        in plain text if you chose to use this service.
+        The security of your Personal Information is important to us, but remember that no method of transmission over
+        the Internet, or method of electronic storage is 100% secure. While we strive to use commercially acceptable
+        means to protect your Personal Information, we cannot guarantee its absolute security. Please remember your
+        password is stored in plain text if you chose to use this service.
       </p>
       <h3>Methods of processing</h3>{" "}
       <p>
-        The Owner takes appropriate security measures to prevent unauthorized
-        access, disclosure, modification, or unauthorized destruction of the
-        Data. The Data processing is carried out using computers and/or IT
-        enabled tools, following organizational procedures and modes strictly
-        related to the purposes indicated. In addition to the Owner, in some
-        cases, the Data may be accessible to certain types of persons in charge,
-        involved with the operation of this Application (administration, sales,
-        marketing, legal, system administration) or external parties (such as
-        third-party technical service providers, mail carriers, hosting
-        providers, IT companies, communications agencies) appointed, if
-        necessary, as Data Processors by the Owner. The updated list of these
+        The Owner takes appropriate security measures to prevent unauthorized access, disclosure, modification, or
+        unauthorized destruction of the Data. The Data processing is carried out using computers and/or IT enabled
+        tools, following organizational procedures and modes strictly related to the purposes indicated. In addition to
+        the Owner, in some cases, the Data may be accessible to certain types of persons in charge, involved with the
+        operation of this Application (administration, sales, marketing, legal, system administration) or external
+        parties (such as third-party technical service providers, mail carriers, hosting providers, IT companies,
+        communications agencies) appointed, if necessary, as Data Processors by the Owner. The updated list of these
         parties may be requested from the Owner at any time.
       </p>
       <h2>Legal basis of processing</h2>
-      <p>
-        The Owner may process Personal Data relating to Users if one of the
-        following applies:
-      </p>
+      <p>The Owner may process Personal Data relating to Users if one of the following applies:</p>
       <ul>
         <li>
-          Users have given their consent for one or more specific purposes.
-          Note: Under some legislations the Owner may be allowed to process
-          Personal Data until the User objects to such processing (“opt-out”),
-          without having to rely on consent or any other of the following legal
-          bases. This, however, does not apply, whenever the processing of
-          Personal Data is subject to European data protection law;
+          Users have given their consent for one or more specific purposes. Note: Under some legislations the Owner may
+          be allowed to process Personal Data until the User objects to such processing (“opt-out”), without having to
+          rely on consent or any other of the following legal bases. This, however, does not apply, whenever the
+          processing of Personal Data is subject to European data protection law;
         </li>
         <li>
-          provision of Data is necessary for the performance of an agreement
-          with the User and/or for any pre-contractual obligations thereof;
+          provision of Data is necessary for the performance of an agreement with the User and/or for any
+          pre-contractual obligations thereof;
+        </li>
+        <li>processing is necessary for compliance with a legal obligation to which the Owner is subject;</li>
+        <li>
+          processing is related to a task that is carried out in the public interest or in the exercise of official
+          authority vested in the Owner;
         </li>
         <li>
-          processing is necessary for compliance with a legal obligation to
-          which the Owner is subject;
-        </li>
-        <li>
-          processing is related to a task that is carried out in the public
-          interest or in the exercise of official authority vested in the Owner;
-        </li>
-        <li>
-          processing is necessary for the purposes of the legitimate interests
-          pursued by the Owner or by a third party.
+          processing is necessary for the purposes of the legitimate interests pursued by the Owner or by a third party.
         </li>
       </ul>
       <p>
-        In any case, the Owner will gladly help to clarify the specific legal
-        basis that applies to the processing, and in particular whether the
-        provision of Personal Data is a statutory or contractual requirement, or
-        a requirement necessary to enter into a contract.
+        In any case, the Owner will gladly help to clarify the specific legal basis that applies to the processing, and
+        in particular whether the provision of Personal Data is a statutory or contractual requirement, or a requirement
+        necessary to enter into a contract.
       </p>
       <h2>Place</h2>
       <p>
-        The Data is processed at the Owner’s operating offices and in any other
-        places where the parties involved in the processing are located.
+        The Data is processed at the Owner’s operating offices and in any other places where the parties involved in the
+        processing are located.
       </p>
       <p>
-        Depending on the User’s location, data transfers may involve
-        transferring the User’s Data to a country other than their own. To find
-        out more about the place of processing of such transferred Data, Users
-        can check the section containing details about the processing of
-        Personal Data.
+        Depending on the User’s location, data transfers may involve transferring the User’s Data to a country other
+        than their own. To find out more about the place of processing of such transferred Data, Users can check the
+        section containing details about the processing of Personal Data.
       </p>
       <p>
-        Users are also entitled to learn about the legal basis of Data transfers
-        to a country outside the European Union or to any international
-        organization governed by public international law or set up by two or
-        more countries, such as the UN, and about the security measures taken by
-        the Owner to safeguard their Data.
+        Users are also entitled to learn about the legal basis of Data transfers to a country outside the European Union
+        or to any international organization governed by public international law or set up by two or more countries,
+        such as the UN, and about the security measures taken by the Owner to safeguard their Data.
       </p>
       <p>
-        If any such transfer takes place, Users can find out more by checking
-        the relevant sections of this document or inquire with the Owner using
-        the information provided in the contact section.
+        If any such transfer takes place, Users can find out more by checking the relevant sections of this document or
+        inquire with the Owner using the information provided in the contact section.
       </p>
       <h2>Retention time</h2>
       <p>
-        Personal Data shall be processed and stored for as long as required by
-        the purpose they have been collected for.
+        Personal Data shall be processed and stored for as long as required by the purpose they have been collected for.
       </p>
       <p>Therefore:</p>
       <ul>
         <li>
-          Personal Data collected for purposes related to the performance of a
-          contract between the Owner and the User shall be retained until such
-          contract has been fully performed.
+          Personal Data collected for purposes related to the performance of a contract between the Owner and the User
+          shall be retained until such contract has been fully performed.
         </li>
         <li>
-          Personal Data collected for the purposes of the Owner’s legitimate
-          interests shall be retained as long as needed to fulfill such
-          purposes. Users may find specific information regarding the legitimate
-          interests pursued by the Owner within the relevant sections of this
-          document or by contacting the Owner.
+          Personal Data collected for the purposes of the Owner’s legitimate interests shall be retained as long as
+          needed to fulfill such purposes. Users may find specific information regarding the legitimate interests
+          pursued by the Owner within the relevant sections of this document or by contacting the Owner.
         </li>
       </ul>
       <p>
-        The Owner may be allowed to retain Personal Data for a longer period
-        whenever the User has given consent to such processing, as long as such
-        consent is not withdrawn. Furthermore, the Owner may be obliged to
-        retain Personal Data for a longer period whenever required to do so for
-        the performance of a legal obligation or upon order of an authority.
+        The Owner may be allowed to retain Personal Data for a longer period whenever the User has given consent to such
+        processing, as long as such consent is not withdrawn. Furthermore, the Owner may be obliged to retain Personal
+        Data for a longer period whenever required to do so for the performance of a legal obligation or upon order of
+        an authority.
       </p>
       <p>
-        Once the retention period expires, Personal Data shall be deleted.
-        Therefore, the right to access, the right to erasure, the right to
-        rectification and the right to data portability cannot be enforced after
-        expiration of the retention period.
+        Once the retention period expires, Personal Data shall be deleted. Therefore, the right to access, the right to
+        erasure, the right to rectification and the right to data portability cannot be enforced after expiration of the
+        retention period.
       </p>
       <h2>Service Providers</h2>
       <p>
-        We may employ third party companies and individuals to facilitate our
-        Service, to provide the Service on our behalf, to perform
-        Service-related services or to assist us in analyzing how our Service is
-        used.
+        We may employ third party companies and individuals to facilitate our Service, to provide the Service on our
+        behalf, to perform Service-related services or to assist us in analyzing how our Service is used.
       </p>
       <p>
-        These third parties have access to your Personal Information only to
-        perform these tasks on our behalf and are obligated not to disclose or
-        use it for any other purpose.
+        These third parties have access to your Personal Information only to perform these tasks on our behalf and are
+        obligated not to disclose or use it for any other purpose.
       </p>
       <h2>
         <strong>The purposes of processing</strong>
       </h2>
       <p>
-        The Data concerning the User is collected to allow the Owner to provide
-        its Services, as well as for the following purposes: Hive.
+        The Data concerning the User is collected to allow the Owner to provide its Services, as well as for the
+        following purposes: Hive.
       </p>
       <p>
-        Users can find further detailed information about such purposes of
-        processing and about the specific Personal Data used for each purpose in
-        the respective sections of this document.
+        Users can find further detailed information about such purposes of processing and about the specific Personal
+        Data used for each purpose in the respective sections of this document.
       </p>
       <h3>Hive</h3>
       <p>
-        This app connects to the Hive api, data including but not limited to
-        your email and password are submitted to Hive. This allows this Service
-        to access your Hive hardware and profile to get a list of devices, set
-        the temperature of your thermostat and get the current tempearture of
-        your thermostat.
+        This app connects to the Hive api, data including but not limited to your email and password are submitted to
+        Hive. This allows this Service to access your Hive hardware and profile to get a list of devices, set the
+        temperature of your thermostat and get the current tempearture of your thermostat.
       </p>
       <a href="https://www.hivehome.com/privacy">Hive Privacy Policy</a>
       <h2>Contact Me</h2>
-      <p>
-        If you have any questions about this Privacy Policy, please contact us.
-        Email: thomas01155@gmail.com
-      </p>
+      <p>If you have any questions about this Privacy Policy, please contact us. Email: thomas01155@gmail.com</p>
     </div>
   );
 };
