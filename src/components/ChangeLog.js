f440d6bb71105c517918157c4f637a8df13e8e2b
thomas <thomas01155@gmail.com>
2020-02-16 21:23:11 +0000

    Update change log

--- src/components/ChangeLog.js
+++ src/components/ChangeLog.js
@@ -5,7 +5,7 @@ const ChangeLog = () => {
     <div className="jumbotron">
         <p>16th Febuary 2020</p>
       <ul>
-        <li>Added support for multiple thermostats <a href="https://github.com/OddBlueDog/HiveTempLockServer/commit/e1d165e7fb56a6a9563cb061cd79de7e5f8ec230">Git</a></li>
+        <li>Added support for multiple thermostats (server version only, local coming soon)<a href="https://github.com/OddBlueDog/HiveTempLockServer/commit/e1d165e7fb56a6a9563cb061cd79de7e5f8ec230">Git</a></li>
       </ul>
     </div>
   );
